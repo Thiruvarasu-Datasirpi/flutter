31871eb68b4218788d69d2eb3636748c81abf910
Thiruvarasu-Datasirpi <thiruvarasu.u@datasirpi.com>
2023-10-05 15:09:09 +0530

    navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,58 @@
 import 'package:flutter/material.dart';
 
-class MyStatefulWidget extends StatefulWidget {
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+void main() {
+  runApp(MyApp());
 }
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _counter = 0; // Example state variable
 
-  void _incrementCounter() {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Page 1'), 
+    Text('Page 2'), 
+    Text('Page 3'), 
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StatefulWidget Example'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Counter Value:',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bottom Navigation Bar Example'),
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Page 1',
             ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 36),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Page 2',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'Page 3',
             ),
           ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
-}
-void main() {
-  runApp(MaterialApp(
-    home: MyStatefulWidget(),
-  ));
 }
