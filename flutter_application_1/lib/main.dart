3e06906c1e115e0b09093fb859febc58e47b9d8d
Thiruvarasu-Datasirpi <thiruvarasu.u@datasirpi.com>
2023-12-05 16:35:54 +0530

    headers done

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -8,76 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      onGenerateRoute: (settings) {
-        // Check the route name and return the appropriate Widget
-        switch (settings.name) {
-          case '/':
-            return MaterialPageRoute(builder: (context) => HomeScreen());
-          case '/details':
-            // Extract arguments from settings.arguments if needed
-            final args = settings.arguments as Map<String, dynamic>;
-            return MaterialPageRoute(builder: (context) => DetailsScreen(args['data']));
-          default:
-            // If the route is not found, you can return an error page or any fallback
-            return MaterialPageRoute(builder: (context) => NotFoundScreen());
-        }
-      },
-      initialRoute: '/',
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          // Title of the app bar
+          title: Text('My Flutter App'),
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the '/details' route with some data
-            Navigator.pushNamed(
-              context,
-              '/details',
-              arguments: {'data': 'Some data for DetailsScreen'},
-            );
-          },
-          child: Text('Go to Details'),
+          // Actions (icons/buttons) on the right side of the app bar
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Handle the search action
+                print('Search pressed');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                // Handle the settings action
+                print('Settings pressed');
+              },
+            ),
+          ],
         ),
-      ),
-    );
-  }
-}
-
-class DetailsScreen extends StatelessWidget {
-  final String data;
-
-  DetailsScreen(this.data);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Details'),
-      ),
-      body: Center(
-        child: Text('Details: $data'),
-      ),
-    );
-  }
-}
-
-class NotFoundScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Not Found'),
-      ),
-      body: Center(
-        child: Text('Route not found!'),
+        body: Center(
+          child: Text('This is the body of the screen'),
+        ),
       ),
     );
   }
