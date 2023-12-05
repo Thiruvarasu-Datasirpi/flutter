2246e65d7481ebb985f6b839f34b9bf4461c1b48
Thiruvarasu-Datasirpi <thiruvarasu.u@datasirpi.com>
2023-12-05 16:42:28 +0530

    titiles done

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -10,31 +10,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          // Title of the app bar
-          title: Text('My Flutter App'),
-
-          // Actions (icons/buttons) on the right side of the app bar
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                // Handle the search action
-                print('Search pressed');
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: () {
-                // Handle the settings action
-                print('Settings pressed');
-              },
-            ),
-          ],
+          title: Text('WELCOME TITLE'),
         ),
-        body: Center(
-          child: Text('This is the body of the screen'),
-        ),
+        body: YourBodyWidget(),
       ),
+    );
+  }
+}
+
+class YourBodyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Your main content goes here
+    return Center(
+      child: Text('Hello, World!'),
     );
   }
 }
