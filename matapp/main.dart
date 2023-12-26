2163742c4730127c5a708bf2e4aa4fb5244e6fdb
Thiruvarasu-Datasirpi <138755453+Thiruvarasu-Datasirpi@users.noreply.github.com>
2023-12-26 15:41:53 +0530

    container done

--- matapp/main.dart
+++ matapp/main.dart
@@ -9,145 +9,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Widgets Demo',
-      home: WidgetDemoScreen(),
+      title: 'Container Example',
+      home: ContainerExample(),
     );
   }
 }
 
-class WidgetDemoScreen extends StatefulWidget {
-  @override
-  _WidgetDemoScreenState createState() => _WidgetDemoScreenState();
-}
-
-class _WidgetDemoScreenState extends State<WidgetDemoScreen> {
-  int selectedValue = 1;
-  bool checkboxValue = false;
-  TextEditingController textController = TextEditingController();
-
+class ContainerExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Widgets Demo'),
+        title: Text('Container Example'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Text widgets
-            Text('Hello, Flutter!'),
-            Text(
-              'Styled text',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            RichText(
-              text: TextSpan(
-                text: 'This is ',
-                style: DefaultTextStyle.of(context).style,
-                children: <TextSpan>[
-                  TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
-                  TextSpan(text: ' and '),
-                  TextSpan(text: 'italic', style: TextStyle(fontStyle: FontStyle.italic)),
-                ],
+      body: Center(
+        child: Container(
+          // Container properties
+          width: 200,
+          height: 200,
+          margin: EdgeInsets.all(16),
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3),
               ),
-            ),
-
-            // Input widgets
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Click me'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Text button'),
-            ),
-            Checkbox(
-              value: checkboxValue,
-              onChanged: (newValue) {
-                setState(() {
-                  checkboxValue = newValue!;
-                });
-              },
-            ),
-            Radio(
-              value: 1,
-              groupValue: selectedValue,
-              onChanged: (newValue) {
-                setState(() {
-                  selectedValue = newValue as int;
-                });
-              },
-            ),
-            TextField(
-              controller: textController,
-              decoration: InputDecoration(
-                labelText: 'Enter text',
-                hintText: 'Type something',
-              ),
-            ),
-
-            // Layout widgets
-            Container(
-              padding: EdgeInsets.all(20),
-              color: Colors.blue,
-              child: Text('Content'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text('Widget 1'),
-                Text('Widget 2'),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Widget 1'),
-                Text('Widget 2'),
-              ],
-            ),
-
-            // Image widget
-
-            // Lists
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text('List item 1'),
-              subtitle: Text('Subtitle 1'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text('List item 2'),
-              subtitle: Text('Subtitle 2'),
-              onTap: () {},
-            ),
-
-            // Progress indicator
-            CircularProgressIndicator(),
-
-            // AlertDialog
-            ElevatedButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: Text('Alert Dialog'),
-                    content: Text('This is an alert dialog.'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text('Close'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              child: Text('Show Alert Dialog'),
-            ),
-          ],
+            ],
+          ),
+          // Child widget
+          child: Text(
+            'Hello, Container!',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
         ),
       ),
     );
