c432f98dd3e49e63b8334a25f51ac9fe227398bc
Thiruvarasu-Datasirpi <138755453+Thiruvarasu-Datasirpi@users.noreply.github.com>
2023-12-26 15:39:22 +0530

    common widgets done

--- matapp/main.dart
+++ matapp/main.dart
@@ -1,41 +1,154 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [Scaffold].
+void main() {
+  runApp(MyApp());
+}
 
-void main() => runApp(const ScaffoldExampleApp());
-
-class ScaffoldExampleApp extends StatelessWidget {
-  const ScaffoldExampleApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ScaffoldExample(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Widgets Demo',
+      home: WidgetDemoScreen(),
     );
   }
 }
 
-class ScaffoldExample extends StatefulWidget {
-  const ScaffoldExample({super.key});
-
+class WidgetDemoScreen extends StatefulWidget {
   @override
-  State<ScaffoldExample> createState() => _ScaffoldExampleState();
+  _WidgetDemoScreenState createState() => _WidgetDemoScreenState();
 }
 
-class _ScaffoldExampleState extends State<ScaffoldExample> {
-  int _count = 0;
+class _WidgetDemoScreenState extends State<WidgetDemoScreen> {
+  int selectedValue = 1;
+  bool checkboxValue = false;
+  TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample Code'),
+        title: Text('Flutter Widgets Demo'),
       ),
-      body: Center(child: Text('You have pressed the button $_count times.')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _count++),
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Text widgets
+            Text('Hello, Flutter!'),
+            Text(
+              'Styled text',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            RichText(
+              text: TextSpan(
+                text: 'This is ',
+                style: DefaultTextStyle.of(context).style,
+                children: <TextSpan>[
+                  TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' and '),
+                  TextSpan(text: 'italic', style: TextStyle(fontStyle: FontStyle.italic)),
+                ],
+              ),
+            ),
+
+            // Input widgets
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click me'),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text('Text button'),
+            ),
+            Checkbox(
+              value: checkboxValue,
+              onChanged: (newValue) {
+                setState(() {
+                  checkboxValue = newValue!;
+                });
+              },
+            ),
+            Radio(
+              value: 1,
+              groupValue: selectedValue,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedValue = newValue as int;
+                });
+              },
+            ),
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                labelText: 'Enter text',
+                hintText: 'Type something',
+              ),
+            ),
+
+            // Layout widgets
+            Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.blue,
+              child: Text('Content'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Widget 1'),
+                Text('Widget 2'),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Widget 1'),
+                Text('Widget 2'),
+              ],
+            ),
+
+            // Image widget
+
+            // Lists
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('List item 1'),
+              subtitle: Text('Subtitle 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('List item 2'),
+              subtitle: Text('Subtitle 2'),
+              onTap: () {},
+            ),
+
+            // Progress indicator
+            CircularProgressIndicator(),
+
+            // AlertDialog
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Alert Dialog'),
+                    content: Text('This is an alert dialog.'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text('Close'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Text('Show Alert Dialog'),
+            ),
+          ],
+        ),
       ),
     );
   }
